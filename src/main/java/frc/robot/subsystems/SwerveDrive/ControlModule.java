be8840a8d0b6ebabc976fe15b7659cb85ba4fc09
Ryan Vu <ryanvu@frc4322.com>
2022-01-30 12:51:14 -0800

    unbun swerve library in-work by Ryan

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive/ControlModule.java
@@ -0,0 +1,189 @@
+package frc.robot.subsystems.SwerveDrive;
+
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PWMSpeedController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * A ControlModule allows you to easily control the speed controllers for 
+ * both rotation and speed</br>
+ * The rotation speed controller has a PIDController with an potentiometer source. 
+ * <i>(TODO: setup an option to use absolute encoders in place of rotation potentiometers)</i></br>
+ * <i>(TODO:Create a TalonSRXModule based on this)</i></br>
+ * 
+ * @author created by Unnas Hussain on 8/2/2017
+ */
+public class ControlModule{
+
+	public WheelPosition position;
+
+	private PWMSpeedController m_rotation; 
+	private PWMSpeedController m_wheel; 
+	
+	private PIDController rotationPID;
+	
+	public double rotateP = 1.0;
+	public double rotateI = 0.0;
+	public double rotateD = 0.0;
+	protected double rotatePIDTolerance = 0.01;
+
+	private AnalogPotentiometer rotationEncoder = null;
+	private Encoder speedEncoder = null;
+
+	protected double previousRate = 0;
+	protected double previousTime = 0;
+	protected double filteredAccel = 0;
+	
+	protected ControlModule(WheelPosition pos) {
+		position = pos;
+	}
+
+	public ControlModule(PWMSpeedController rotation, PWMSpeedController wheel, int potentiometerPort, WheelPosition pos){
+		position = pos;
+
+		m_rotation = rotation;
+		m_wheel = wheel;
+		rotationEncoder = new AnalogPotentiometer(potentiometerPort, 360.0, 0);
+
+		rotationPID = new PIDController(rotateP, rotateI, rotateD, rotationEncoder, m_rotation);
+
+		rotationPID.setContinuous();
+		rotationPID.setAbsoluteTolerance(rotatePIDTolerance);
+	}
+
+	public ControlModule(PWMSpeedController rotation, PWMSpeedController wheel, int encoderPort, int wheelEncoderA, int wheelEncoderB, WheelPosition location){
+		this(rotation, wheel, encoderPort, location);
+		speedEncoder = new Encoder(wheelEncoderA, wheelEncoderB);
+	}
+
+	public void setSpeedAndAngle(Joystick drive, Joystick rotate){
+		m_wheel.set(SwerveHelper.getSpeedValue(drive, rotate, position.wheelNumber));
+		rotationPID.setSetpoint(SwerveHelper.getAngleValue(drive, rotate, position.wheelNumber));
+	}
+
+	public void setEncoder(int encoderPortA, int encoderPortB){
+		this.speedEncoder = new Encoder(encoderPortA, encoderPortB);
+	}
+
+	//*********************PID Helper Methods******************//
+
+	public void setRotationPIDTolerance(double tolerance) {
+		rotatePIDTolerance = tolerance;
+		rotationPID.setAbsoluteTolerance(tolerance);
+	}
+	
+	public void setRotationPID(double kp, double ki, double kd){
+		rotationPID.setPID(kp, ki, kd);
+	}
+
+	public void setRotationPIDF(double kp, double ki, double kd, double kf){
+		rotationPID.setPID(kp, ki, kd, kf);
+	}
+
+	public double getRotationP(){
+		return rotationPID.getP();
+	}
+
+	public double getRotationI(){
+		return rotationPID.getI();
+	}
+
+	public double getRotationD(){
+		return rotationPID.getD();
+	}
+
+	public double getRotationF(){
+		return rotationPID.getF();
+	}
+
+	public void resetSpeedEncoder(){
+		if(speedEncoder != null)
+			this.speedEncoder.reset();
+	}
+
+	//************** Pos/Vel/Acc Helper Methods **************// 
+
+	public double getAngle(){
+		if(rotationEncoder != null)
+			return rotationEncoder.get();
+		return -1.0;
+	}
+
+	public double getDistance() {
+		if(speedEncoder != null)
+			return speedEncoder.getDistance();
+		return 0.0;
+	}
+
+	public double getVelocity() {
+		if(speedEncoder != null)
+			return speedEncoder.getRate();
+		return 0.0;
+
+	}
+
+	public double getAcceleration() {
+
+		double currentRate = this.getVelocity();
+		double currentTime = Timer.getFPGATimestamp();
+
+		double acceleration = (currentRate - previousRate) / (currentTime - previousTime);
+
+		previousRate = currentRate;
+		previousTime = currentTime;
+
+		filteredAccel = acceleration * 0.5 + filteredAccel * 0.5; //dampens random spikes due to the fact that we are deriving this value
+
+		return filteredAccel;
+	}	
+
+	public Encoder getSpeedEncoder(){
+		if(speedEncoder != null) 
+			return this.speedEncoder;
+		return null;
+	}
+
+	public AnalogPotentiometer getRotationEncoder() {
+		if(rotationEncoder != null)
+			return this.rotationEncoder;
+		return null;
+	}
+	
+	public void debug() {
+		String name = position.toString() + " ControlModule/";
+		
+		SmartDashboard.putNumber(name + "Distance", this.getDistance());
+		SmartDashboard.putNumber(name + "Velocity", this.getVelocity());
+		SmartDashboard.putNumber(name + "Acceleration", this.getAcceleration());
+		
+		SmartDashboard.putNumber(name + "Spin P", this.getRotationP());
+		SmartDashboard.putNumber(name + "Spin I", this.getRotationI());
+		SmartDashboard.putNumber(name + "Spin D", this.getRotationD());
+		SmartDashboard.putNumber(name + "Spin F", this.getRotationF());
+	}
+
+	public enum WheelPosition{
+		FRONT_RIGHT(0), FRONT_LEFT(1), BACK_LEFT(2), BACK_RIGHT(3);
+
+		public int wheelNumber;
+
+		WheelPosition(int id){
+			wheelNumber = id;
+		}
+
+		public String toString() {
+			switch(wheelNumber) {
+			case 0: return "FRONT_RIGHT";
+			case 1: return "FRONT_LEFT";
+			case 2: return "BACK_LEFT";
+			case 3: return "BACK_RIGHT";
+			default: return "???";
+			}
+		}
+	}
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveDrive/SwerveHelper.java
@@ -0,0 +1,192 @@
+package frc.robot.subsystems.SwerveDrive;
+
+import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * Calculates the angle and speed of each 4 wheels based
+ * on the input from two 2-axis joysticks.</br>
+ * Kinematics from <a href = https://www.chiefdelphi.com/media/papers/2426)> Ether</a>
+ * 
+ * @author created by Unnas Hussain on 8/2/2017
+ */
+public class SwerveHelper {
+
+	public static double WHEELBASE_LENGTH = 36.0/12.0;  //based on 2791's robot Stoker from FRC SteamWorks
+	public static double WHEELBASE_WIDTH = 28.0/12.0;   //based on 2791's robot Stoker from FRC SteamWorks
+
+	/*
+	 * Math Conventions:
+	 * 
+	 * 
+	 * Wheel ID Numbers:
+	 * 0 = front_right;
+	 * 1 = front_left;
+	 * 2 = rear_left;
+	 * 3 = rear_right;
+	 * 
+	 * 0.0 degrees is the wheels facing the front of the bot
+	 * 
+	 */
+	
+	
+	protected static double[] wheelSpeed = new double[4];
+	protected static double[] wheelAngle = new double[4];
+
+	protected static GyroBase m_gyro = null;
+
+	public static boolean fieldCentric = true;
+
+	public static boolean useAngleToReverse = true;
+
+
+	public static void setRobotDimensions(double length, double width) {
+		WHEELBASE_LENGTH = length;
+		WHEELBASE_WIDTH = width; 
+	}
+	
+	/**
+	 * 
+	 * @return {wheelbase length, wheelbase width}
+	 */
+	public static double[] getRobotDimensions() {		
+		return new double[] {WHEELBASE_LENGTH, WHEELBASE_WIDTH};
+	}
+	
+	public static double getSpeedValue(Joystick drive, Joystick rotate, int wheelID){
+		calculate(drive.getY(), drive.getX(), rotate.getX());
+		return wheelSpeed[wheelID];
+	}
+
+	public static double getAngleValue(Joystick drive, Joystick rotate,  int wheelID){
+		calculate(drive.getY(), drive.getX(), rotate.getX());
+		return wheelAngle[wheelID];
+	}	
+
+	public static void calculate(double forward, double strafe, double rotate){
+
+		if(fieldCentric && getGyro() != null){
+			double gyroAngle =Math.toRadians(getGyro().getAngle());
+
+			double temp = forward * Math.cos(gyroAngle) + strafe*Math.sin(gyroAngle);
+			strafe = -forward*Math.sin(gyroAngle) + strafe*Math.cos(gyroAngle);
+			forward = temp;
+		}else if (fieldCentric && getGyro() == null) {
+			System.out.println ("No Gyro found, Swerve calculations will be Robot Centric");
+		}
+
+		/*
+		 * Inverse Kinematics Based on work from Ether on Cheif Delphi
+		 */
+		double frontRightX,frontRightY,backLeftX,backLeftY;
+		double L = WHEELBASE_LENGTH, W = WHEELBASE_WIDTH;
+		double R = Math.sqrt(Math.pow(L, 2) + Math.pow(W, 2) );
+
+		frontRightX = strafe - rotate*(L/R);
+		frontRightY = strafe + rotate*(L/R);
+		backLeftX = forward - rotate*(W/R);
+		backLeftY = forward + rotate*(W/R);
+
+		wheelSpeed[0] = Math.sqrt((Math.pow(frontRightY,2) + Math.pow(backLeftX,2)));
+		wheelSpeed[1] = Math.sqrt((Math.pow(frontRightY,2) + Math.pow(backLeftY,2)));
+		wheelSpeed[2] = Math.sqrt((Math.pow(frontRightX,2) + Math.pow(backLeftY,2)));
+		wheelSpeed[3] = Math.sqrt((Math.pow(frontRightX,2) + Math.pow(backLeftX,2)));
+		normalizeSpeeds();
+
+		wheelAngle[0] = Math.atan2(frontRightY, backLeftX) * 180/Math.PI;
+		wheelAngle[1] = Math.atan2(frontRightY, backLeftY) * 180/Math.PI;
+		wheelAngle[2] = Math.atan2(frontRightX, backLeftY) * 180/Math.PI;
+		wheelAngle[3] = Math.atan2(frontRightX, backLeftX) * 180/Math.PI;
+
+		if(!useAngleToReverse)
+			reverseWithSpeed();
+
+	}
+
+	/**
+	 * The kinematics create backwards vectors by rotating the wheels and always driving in a positive direction,
+	 * (So going backwards would be Speed == 1.0, Angle == 180) </br>
+	 * 
+	 * If the vehicle design is such that it would better to avoid rotating the modules,
+	 * use this method after the kinematics are done.
+	 * (Then going backwards will be Speed ==-1.0, Angle == 0)
+	 */
+	private static void reverseWithSpeed(){
+		double temp_wheelAngle[] = wheelAngle;
+
+		if(temp_wheelAngle[0] > 90.0 || temp_wheelAngle[0] < -90.0){
+			wheelAngle[0] = temp_wheelAngle[0] - Math.copySign(180.0, temp_wheelAngle[0]);
+			wheelSpeed[0] *= -1.0;
+		}
+
+		if(temp_wheelAngle[1] > 90.0 || temp_wheelAngle[1] < -90.0){
+			wheelAngle[1] = temp_wheelAngle[1] - Math.copySign(180.0, temp_wheelAngle[1]);
+			wheelSpeed[1] *= -1.0;
+		}
+
+		if(temp_wheelAngle[2] > 90.0 || temp_wheelAngle[2] < -90.0){
+			wheelAngle[2] = temp_wheelAngle[2] - Math.copySign(180.0, temp_wheelAngle[2]);
+			wheelSpeed[2] *= -1.0;
+		}
+
+		if(temp_wheelAngle[3] > 90.0 || temp_wheelAngle[3] < -90.0){
+			wheelAngle[3] = temp_wheelAngle[3] - Math.copySign(180.0, temp_wheelAngle[0]);
+			wheelSpeed[3] *= -1.0;
+		}
+	}
+
+
+	/**Normalize speeds to be in a range from 0 to +1.0*/
+	private static void normalizeSpeeds(){
+		double max = Math.max(wheelSpeed[0], wheelSpeed[1]);
+		max = Math.max(max, wheelSpeed[2]);
+		max = Math.max(max, wheelSpeed[3]);
+
+		if(max>1){
+			wheelSpeed[0] /=max;
+			wheelSpeed[1] /=max;
+			wheelSpeed[2] /=max;
+			wheelSpeed[3] /=max;
+		}
+	}
+
+	public static void setToFieldCentric(){
+		fieldCentric = true;
+	}
+
+	public static void setToBotCentric(){
+		fieldCentric = false;
+	}
+
+	
+	/**
+	 * Reversing with Rotation means that when you go backwards,
+	 * the speed will be in a positive direction but the angle will spin.
+	 * This means that the speed is never negative </br>
+	 * (So going backwards would be Speed == 1.0 , Angle == 180)
+	 */
+	public static void setReversingToRotation(){
+		useAngleToReverse = true;
+	}
+
+	/**
+	 * Reversing with Speed means that when you go backwards,
+	 * the speed will be in a negative direction and there will be less rotation.
+	 * This means that the angle is never outside the range -90 deg to 90 deg </br>
+	 * (So going backwards would be Speed == -1.0 , Angle == 0)
+	 */
+	public static void setReversingToSpeed(){
+		useAngleToReverse = false;
+	}
+
+	public static void setGyro(GyroBase gyro){
+		m_gyro = gyro;
+		fieldCentric = true;
+	}
+
+	public static GyroBase getGyro(){
+		return m_gyro;
+	}
+
+}
+
