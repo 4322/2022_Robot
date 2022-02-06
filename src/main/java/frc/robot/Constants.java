ac13e42bfd1ef5941e1104e0f2348cbdb0d358c8
Aaron Trinh <aarontrinh@frc4322.com>
2022-02-06 10:15:41 -0800

    Set initial kP, kI, and KD values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -22,7 +26,7 @@ public final class Constants {
         public static final int frontRightDriveID = 2;
         public static final int frontRightRotationID = 3;
         public static final int rearRightDriveID = 4;
-        public static final int rearRightRotationID = 5;      
+        public static final int rearRightRotationID = 5;
         public static final int frontLeftDriveID = 6;
         public static final int frontLeftRotationID = 7;
         public static final int rearLeftDriveID = 8;
@@ -36,16 +40,15 @@ public final class Constants {
         public static final double kModuleMaxAngularAcceleration = 2 * Math.PI;
 
         public final class SwerveModuleConstants {
-            
-            //Nothing for now
+
+            // Nothing for now
 
         }
 
         public final class PID_Values {
-            public static final double kP = 1;
-            public static final double kI = 1;
-            public static final double kD = 1;
-            public static final double kF = 1;
+            public static final double kP = 0;
+            public static final double kI = 0;
+            public static final double kD = 0;
         }
     }
 }
