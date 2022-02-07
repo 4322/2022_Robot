10d803626a4c0ceb00cf69190c588fa8adfefe39
Ryan Vu <ryanvu@frc4322.com>
2022-02-06 18:47:10 -0800

    Removed Unncessary Imports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,9 +4,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
