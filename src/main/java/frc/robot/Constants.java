722df7839de38c40dcc69d17fe3a583a2cbb02cb
Zayd Khan <zaydkhan@frc4322.com>
2022-02-13 10:52:46 -0800

    created new Shooter class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,26 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final boolean debug = true;
+
+    public static final class ShooterConstants {
+
+        public static final boolean shooterEnabled = true;
+
+        public static final int flywheelOneID = 0;
+        public static final int flywheelTwoID = 0;
+
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kIz = 0;
+        public static final double kFF = 0;
+        public static final double kMinRange = 0;
+        public static final double kMaxRange = 0;
+        public static final double rampRate = 0;
+
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+
+  private ShuffleboardTab tab
+
+  private CANSparkMax flywheelOne;
+  private CANSparkMax flywheelTwo;
+
+  private RelativeEncoder flywheelEncoder;
+  private SparkMaxPIDController flywheelPID;
+
+  public Shooter() {
+    flywheelOne = new CANSparkMax(Constants.ShooterConstants.flywheelOneID, MotorType.kBrushless);
+    flywheelTwo = new CANSparkMax(Constants.ShooterConstants.flywheelTwoID, MotorType.kBrushless);
+
+    flywheelOne.restoreFactoryDefaults();
+    flywheelOne.setInverted(true);
+    flywheelTwo.restoreFactoryDefaults();
+    flywheelTwo.follow(flywheelOne, true);
+    flywheelOne.setIdleMode(IdleMode.kCoast);
+    flywheelTwo.setIdleMode(IdleMode.kCoast);
+    flywheelOne.setClosedLoopRampRate(Constants.ShooterConstants.rampRate);  // don't eject the shooter
+
+    flywheelEncoder = flywheelOne.getEncoder();
+    flywheelPID = flywheelOne.getPIDController();
+
+    flywheelPID.setP(Constants.ShooterConstants.kP);
+    flywheelPID.setI(Constants.ShooterConstants.kI);
+    flywheelPID.setD(Constants.ShooterConstants.kD);
+    flywheelPID.setIZone(Constants.ShooterConstants.kIz);
+    flywheelPID.setFF(Constants.ShooterConstants.kFF);
+    flywheelPID.setOutputRange(Constants.ShooterConstants.kMinRange, Constants.ShooterConstants.kMaxRange);
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+
+}
