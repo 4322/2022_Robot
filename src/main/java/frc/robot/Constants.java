1370348c39bb7532d816aa32d330d8902a641018
Zayd Khan <zaydkhan@frc4322.com>
2022-02-24 19:41:28 -0800

    put in absolute offset values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -82,10 +82,10 @@ public final class Constants {
             public static final double[] CANCoderOffsetDegrees;
             static {
                 CANCoderOffsetDegrees = new double[4];
-                CANCoderOffsetDegrees[WheelPosition.FRONT_RIGHT.wheelNumber] = 0;
-                CANCoderOffsetDegrees[WheelPosition.FRONT_LEFT.wheelNumber] = 0;
-                CANCoderOffsetDegrees[WheelPosition.BACK_RIGHT.wheelNumber] = 0;
-                CANCoderOffsetDegrees[WheelPosition.BACK_LEFT.wheelNumber] = 0;
+                CANCoderOffsetDegrees[WheelPosition.FRONT_RIGHT.wheelNumber] = 78.311;
+                CANCoderOffsetDegrees[WheelPosition.FRONT_LEFT.wheelNumber] = 169.365;
+                CANCoderOffsetDegrees[WheelPosition.BACK_RIGHT.wheelNumber] = 106.787;
+                CANCoderOffsetDegrees[WheelPosition.BACK_LEFT.wheelNumber] = 106.787;
             }
         }
 
