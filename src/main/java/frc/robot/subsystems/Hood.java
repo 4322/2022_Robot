efea7967e9fce15333048bf6cd78d4713d449df9
Ryan Vu <ryanvu@frc4322.com>
2022-02-27 08:45:14 -0800

    Started Hood Code

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -8,10 +9,11 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Hood extends SubsystemBase{
     
-    private WPI_TalonSRX hood;
+    private WPI_TalonFX hood;
 
     //SHUFFLEBOARD
     private ShuffleboardTab tab = Shuffleboard.getTab("Hood");
@@ -23,8 +25,19 @@ public class Hood extends SubsystemBase{
     .withSize(1,1)
     .getEntry();
 
-    /*public Hood() {
-        hood = new WPI_TalonSRX()
+    public Hood() {
+        hood = new WPI_TalonFX(Constants.HoodConstants.hoodTalon_ID);
 
-    }*/
+        hood.configFactoryDefault();
+        hood.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,
+                                                  Constants.HoodConstants.kPIDLoopIdx,
+                                                  Constants.HoodConstants.kTimeoutMs);
+        hood.setSensorPhase(Constants.HoodConstants.kSensorPhase);
+    
+        /* Config the peak and nominal outputs */
+            hood.configNominalOutputForward(Constants.HoodConstants.minForwardPower, Constants.HoodConstants.kTimeoutMs);
+            hood.configNominalOutputReverse(Constants.HoodConstants.minReversePower, Constants.HoodConstants.kTimeoutMs);
+            hood.configPeakOutputForward(Constants.HoodConstants.maxForwardPower, Constants.HoodConstants.kTimeoutMs);
+            hood.configPeakOutputReverse(Constants.HoodConstants.maxReversePower, Constants.HoodConstants.kTimeoutMs);        
+    }
 }
