ae2ed4c1a6727af12d34a4d12dd1232eee632d20
Zayd Khan <zaydkhan@frc4322.com>
2022-02-27 09:41:05 -0800

    added necessary commands and imported XboxController

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,13 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.Drive_Manual;
+import frc.robot.commands.Shooter_SetPower;
+import frc.robot.commands.Shooter_Stop;
 import frc.robot.subsystems.Driveunbun;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -23,10 +26,18 @@ public class RobotContainer {
   public static Joystick pilotl = new Joystick(0);
   public static Joystick pilotr = new Joystick(1);
 
+  public static XboxController coPilot = new XboxController(2);
+
   // The robot's subsystems and commands are defined here...
   private final Driveunbun driveunbun = new Driveunbun();
+  private final Shooter shooter = new Shooter();
 
+  // Drive Commands
   private final Drive_Manual driveManual = new Drive_Manual(driveunbun);
+
+  // Shooter Commands
+  private final Shooter_Stop stopShooter = new Shooter_Stop(shooter);
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -50,7 +61,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    coPilot.b.whenPressed(new Shooter_SetPower(shooter, 3000));
+    coPilot.a.whenPressed(stopShooter);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/commands/Shooter_SetPower.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class Shooter_SetPower extends CommandBase {
+  /**
+   * Creates a new Enable_Shooter_Power.
+   */
+
+  private Shooter shooter;
+  private double m_rpm;
+
+
+  public Shooter_SetPower(Shooter shooterSubsystem, double rpm) {
+    shooter = shooterSubsystem;
+    m_rpm = rpm;
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    shooter.setSpeed(m_rpm);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  
+  }
+
+  // Terminates command immediately
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
