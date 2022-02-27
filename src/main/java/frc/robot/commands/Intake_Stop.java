8bb12df8edfe74796611480f04166555770e8bff
Ryan Vu <ryanvu@frc4322.com>
2022-02-27 11:45:12 -0800

    Added Intake_Stop command

--- src/main/java/frc/robot/commands/Intake_Stop.java
+++ src/main/java/frc/robot/commands/Intake_Stop.java
@@ -1,5 +1,41 @@
 package frc.robot.commands;
 
-public class Intake_Stop {
-    
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class Intake_Stop extends CommandBase {
+  /**
+   * Creates a new Collector_Stop.
+   */
+
+   private Intake intake;
+
+  public Intake_Stop(Intake intakeSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    intake = intakeSubsystem;
+    addRequirements(intake);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.stop();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
