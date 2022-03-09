7403bd22769c806b843e1033c8331474b8f7a933
Zayd Khan <zaydkhan@frc4322.com>
2022-03-08 20:55:07 -0800

    removed unused ultrasonic code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -37,9 +36,6 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    if (Constants.ballSensorEnabled) {
-      Ultrasonic.setAutomaticMode(true);
-    }
 
     tab = Shuffleboard.getTab("Enabled Subsystems");
 
