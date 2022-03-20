6934f74ee71164607f936bc79629ebc3936bc9dd
Ryan Vu <ryanvu@frc4322.com>
2022-03-20 10:44:33 -0700

    Changed shooter PID Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -163,9 +163,9 @@ public final class Constants {
 
         public static final double kP = .00025;
         public static final double kI = 0.000001;
-        public static final double kD = 0.004;
-        public static final double kIz = 300;
-        public static final double kFF = 0.00015;
+        public static final double kD = 0.008;
+        public static final double kIz = 200;
+        public static final double kFF = 0.00019;
         public static final double kMinRange = 0;   // let flywheel coast down, don't apply power to slow it
         public static final double kMaxRange = 1.0;
         public static final double rampRate = 1.0;  // seconds to go from stopped to full power
