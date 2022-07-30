6bf4d14e2a723985228f30fb67962c84f63454fa
Mili Shah <milishah@frc4322.com>
2022-07-30 12:01:47 -0700

    reworked logic in ClimbVertical

--- src/main/java/frc/robot/commands/ClimbVertical.java
+++ src/main/java/frc/robot/commands/ClimbVertical.java
@@ -14,16 +14,8 @@ import frc.robot.Constants.ClimberConstants;
 import frc.robot.subsystems.Climber;
 
 public class ClimbVertical extends CommandBase {
-
-  private climberMode currentMode;
   private final Climber climber;
   private Timer overrideTimer = new Timer();
-
-  public enum climberMode {
-    stopped,
-    forwardFirstBar,
-    done;
-  }
 
   public ClimbVertical(Climber climbsubsystem) {
     climber = climbsubsystem;
@@ -35,29 +27,9 @@ public class ClimbVertical extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    climber.setCurrentPosition(0);
-    currentMode = climberMode.stopped;
     overrideTimer.reset();
     overrideTimer.start();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    switch (currentMode) {
-      case stopped:
-        climber.moveToPosition(ClimberConstants.forwardFirstBar);
-        currentMode = climberMode.forwardFirstBar;
-      break;
-      case forwardFirstBar:
-        if (climber.isAtTarget()) {
-          climber.stop();
-          currentMode = climberMode.done;
-        }
-      break;
-      case done:
-        break;
-    }
+    climber.moveToPosition(ClimberConstants.forwardFirstBar);
   }
 
   // Called once the command ends or is interrupted.
@@ -70,7 +42,7 @@ public class ClimbVertical extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return ((currentMode == climberMode.done) || (overrideTimer.hasElapsed(ClimberConstants.overrideTime)));
+    return ((climber.isAtTarget()) || (overrideTimer.hasElapsed(ClimberConstants.overrideTime)));
   }
 
 }
