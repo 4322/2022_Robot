edef77fc2bf1626efad7616d2529e284b8d5d4fd
Ryan Vu <ryanvu@frc4322.com>
2022-09-21 18:42:01 -0700

    Created ClimberEngage command and bind it to right dPad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,6 +90,7 @@ public class RobotContainer {
   private final ClimbVertical climbVertical = new ClimbVertical(climber);
   private final ClimbManual climbManual = new ClimbManual(climber);
   private final ClimberReset climberReset = new ClimberReset(climber);
+  private final ClimberEngage climberEngage = new ClimberEngage(climber, driveunbun);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
 
@@ -197,6 +198,7 @@ public class RobotContainer {
       coPilot.start.whenPressed(climbAuto);
       coPilot.dPad.down.whenPressed(climbStop);
       coPilot.dPad.up.whenPressed(climbVertical);
+      coPilot.dPad.right.whenHeld(climberEngage);
     }
     
     //coPilot.dPad.up.whileHeld(new CalcFiringSolution(kicker, shooter, hood, limelight));
