c45091977501e166dd3bb91245c1d56da61f35e0
Ryan Vu <ryanvu@frc4322.com>
2022-09-21 17:14:20 -0700

    Made ClimbHorizontal Command and binded to coPilot left dPad

---
+++ src/main/java/frc/robot/commands/ClimbHorizontal.java
@@ -0,0 +1,49 @@
+/*
+
+Horizontal Position
+Forward rotation (toward front) until hit bar
+Reposition robot to line up hooks
+
+*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ClimberConstants;
+import frc.robot.subsystems.Climber;
+
+public class ClimbHorizontal extends CommandBase {
+  private final Climber climber;
+  private Timer overrideTimer = new Timer();
+  private boolean abort;
+
+  public ClimbHorizontal(Climber climbsubsystem) {
+    climber = climbsubsystem;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(climber);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    overrideTimer.reset();
+    overrideTimer.start();
+    abort = !climber.moveToPosition(ClimberConstants.horizontal, Climber.climbMode.unloaded);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    climber.stop();
+    climber.unlockClimb();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return ((climber.isAtTarget()) || (overrideTimer.hasElapsed(ClimberConstants.overrideTime)) || (abort));
+  }
+
+}
