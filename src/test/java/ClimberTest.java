d72823592d54ed6a5c1d052d20d741f127e95d93
Zayd Khan <zaydkhan@frc4322.com>
2022-09-25 11:23:08 -0700

    example of unit testing working

---
+++ src/test/java/ClimberTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ClimberTest {
+  
+  @Test
+  public void testOnePlusOne_Two() {
+    assertEquals(1+1, 2);
+  }
+
+  @Test
+  public void testOnePlusOne_Three() {
+    assertEquals(1+1, 3);
+  }
+
+}
