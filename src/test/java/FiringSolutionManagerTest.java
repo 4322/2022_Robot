a9daf2dfe7fc9a74773ae4583dcf9c9e9d9dcad0
zaydkhan@frc4322.com <zaydkhan@frc4322.com>
2022-10-29 11:44:21 -0700

    added new firing solution manager test

--- src/test/java/FiringSolutionManagerTest.java
+++ src/test/java/FiringSolutionManagerTest.java
@@ -21,4 +21,26 @@ public class FiringSolutionManagerTest {
     public void testCalcNewSolutionExisting_insideTarmac() {
         assertEquals(manager.calcNewSolution(53), Constants.FiringSolutions.insideTarmac);
     }
+
+    @Test
+    public void testCalcNewSolution_insideMiddleTarmac() {
+        FiringSolution insideTarmac = Constants.FiringSolutions.insideTarmac;
+        FiringSolution middleTarmac = Constants.FiringSolutions.middleTarmac;
+
+        double newKickerSpeed = (insideTarmac.getKickerSpeed() + middleTarmac.getKickerSpeed()) / 2;
+        double newFlywheelSpeed = (insideTarmac.getFlywheelSpeed() + middleTarmac.getFlywheelSpeed()) / 2;
+        double newHoodPosition = (insideTarmac.getHoodPosition() + middleTarmac.getHoodPosition()) / 2;
+        double newAimingTolerance = (insideTarmac.getAimingToleranceDeg() + middleTarmac.getAimingToleranceDeg()) / 2;
+        double newDistance = (insideTarmac.getDistance() + middleTarmac.getDistance()) / 2;
+
+        FiringSolution insideMiddleTarmac = new FiringSolution(
+            newKickerSpeed, 
+            newFlywheelSpeed, 
+            newHoodPosition,
+            newAimingTolerance,
+            newDistance
+        );
+
+        assertEquals(manager.calcNewSolution(newDistance), insideMiddleTarmac);
+    }
 }
