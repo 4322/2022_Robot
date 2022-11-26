5902388f2ef7276ac5f4fbabf169d1fd3ad5daf8
zaydkhan@frc4322.com <zaydkhan@frc4322.com>
2022-11-26 13:19:17 -0800

    above minimum below maximum tests

--- src/test/java/FiringSolutionManagerTest.java
+++ src/test/java/FiringSolutionManagerTest.java
@@ -17,21 +17,48 @@ public class FiringSolutionManagerTest {
         return; // nothing to shut down
     }
 
+    // Test a solution for an existing firing solution (should give back the same firing solution)
     @Test 
     public void testCalcNewSolutionExisting_insideTarmac() {
-        assertEquals(manager.calcNewSolution(53), Constants.FiringSolutions.insideTarmac);
+        
+        assertEquals(manager.calcNewSolution(Constants.FiringSolutions.insideTarmac.getDistance()),
+        Constants.FiringSolutions.insideTarmac);
+    }
+
+    // Test a solution for a distance value that is below the shortest firing solution
+    // (should give back lowest firing solution, in this case middleTarmac)
+    @Test
+    public void testCalcNewSolutionBelowMinimum_middleTarmac() {
+        assertEquals(manager.calcNewSolution(0), Constants.FiringSolutions.middleTarmac);
+    }
+
+    // Test a solution for a distance value that is above the longest firing solution
+    // (should give back highest firing solution, in this case reallyFar)
+    @Test
+    public void testCalcNewSolutionAboveMaximum_reallyFar() {
+        assertEquals(manager.calcNewSolution(1000), Constants.FiringSolutions.reallyFar);
     }
 
     @Test
     public void testCalcNewSolution_insideMiddleTarmac() {
+        FiringSolution insideTarmac = Constants.FiringSolutions.insideTarmac;
+        FiringSolution middleTarmac = Constants.FiringSolutions.middleTarmac;
+
+        // Since this is the midpoint, find the average between the two values for the new firing solution
+        double newKickerSpeed = (insideTarmac.getKickerSpeed() + middleTarmac.getKickerSpeed()) / 2;
+        double newFlywheelSpeed = (insideTarmac.getFlywheelSpeed() + middleTarmac.getFlywheelSpeed()) / 2;
+        double newHoodPosition = (insideTarmac.getHoodPosition() + middleTarmac.getHoodPosition()) / 2;
+        double newAimingTolerance = (insideTarmac.getAimingToleranceDeg() + middleTarmac.getAimingToleranceDeg()) / 2;
+        double newDistance = (insideTarmac.getDistance() + middleTarmac.getDistance()) / 2;
+
         FiringSolution insideMiddleTarmac = new FiringSolution(
-            2200, 
-            3050, 
-            2650,
-            2.0,
-            48.8
+            newKickerSpeed, 
+            newFlywheelSpeed, 
+            newHoodPosition,
+            newAimingTolerance,
+            newDistance
         );
 
-        assertEquals(manager.calcNewSolution(48.8), insideMiddleTarmac);
+        assertEquals(manager.calcNewSolution(newDistance), insideMiddleTarmac);
     }
 }
