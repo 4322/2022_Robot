e89fc3fa529134d60fdb81d47d1a295d7f1f86a8
zaydkhan4322 <zaydkhan@frc4322.com>
2022-12-27 11:18:36 -0800

    formatting

--- src/main/java/frc/robot/FiringSolution/FiringSolution.java
+++ src/main/java/frc/robot/FiringSolution/FiringSolution.java
@@ -1,49 +1,67 @@
 package frc.robot.FiringSolution;
 
 import frc.robot.utility.OrangeMath;
+
 public class FiringSolution {
-    private double flywheelSpeed;
-    private double hoodPosition;
-    private double kickerSpeed;
-    private double aimingToleranceDeg;
-    private double distance;
+  private double flywheelSpeed;
+  private double hoodPosition;
+  private double kickerSpeed;
+  private double aimingToleranceDeg;
+  private double distance;
 
-    public FiringSolution(double kickerSpeed, double flywheelSpeed, double hoodPosition, double aimingToleranceDeg, double distanceIn) {
-      this.kickerSpeed = kickerSpeed;
-      this.flywheelSpeed = flywheelSpeed;
-      this.hoodPosition = hoodPosition;
-      this.aimingToleranceDeg = aimingToleranceDeg;
-      this.distance = distanceIn;
+  public FiringSolution(double kickerSpeed, double flywheelSpeed, double hoodPosition,
+      double aimingToleranceDeg, double distanceIn) {
+    this.kickerSpeed = kickerSpeed;
+    this.flywheelSpeed = flywheelSpeed;
+    this.hoodPosition = hoodPosition;
+    this.aimingToleranceDeg = aimingToleranceDeg;
+    this.distance = distanceIn;
+  }
+
+  // See: https://www.geeksforgeeks.org/overriding-equals-method-in-java/
+  @Override
+  public boolean equals(Object o) {
+
+    // If the object is compared with itself then return true
+    if (o == this) {
+      return true;
     }
 
-    // See: https://www.geeksforgeeks.org/overriding-equals-method-in-java/
-    @Override
-    public boolean equals(Object o) {
- 
-        // If the object is compared with itself then return true 
-        if (o == this) {
-            return true;
-        }
- 
-        /* Check if o is an instance of FiringSolution or not
-          "null instanceof [type]" also returns false */
-        if (!(o instanceof FiringSolution)) {
-            return false;
-        }
-         
-        // typecast o to FiringSolution so that we can compare data members
-        FiringSolution c = (FiringSolution) o;
-         
-        return OrangeMath.equalToTwoDecimal(c.getAimingToleranceDeg(), aimingToleranceDeg)
-              && OrangeMath.equalToTwoDecimal(c.getDistance(), distance)
-              && OrangeMath.equalToTwoDecimal(c.getFlywheelSpeed(), flywheelSpeed)
-              && OrangeMath.equalToTwoDecimal(c.getHoodPosition(), hoodPosition)
-              && OrangeMath.equalToTwoDecimal(c.getKickerSpeed(), kickerSpeed);
+    /*
+     * Check if o is an instance of FiringSolution or not "null instanceof [type]" also returns
+     * false
+     */
+    if (!(o instanceof FiringSolution)) {
+      return false;
     }
 
-    public double getFlywheelSpeed() { return flywheelSpeed; }
-    public double getKickerSpeed() { return kickerSpeed; }
-    public double getHoodPosition() { return hoodPosition; }
-    public double getAimingToleranceDeg() { return aimingToleranceDeg; }
-    public double getDistance() { return distance; }
+    // typecast o to FiringSolution so that we can compare data members
+    FiringSolution c = (FiringSolution) o;
+
+    return OrangeMath.equalToTwoDecimal(c.getAimingToleranceDeg(), aimingToleranceDeg)
+        && OrangeMath.equalToTwoDecimal(c.getDistance(), distance)
+        && OrangeMath.equalToTwoDecimal(c.getFlywheelSpeed(), flywheelSpeed)
+        && OrangeMath.equalToTwoDecimal(c.getHoodPosition(), hoodPosition)
+        && OrangeMath.equalToTwoDecimal(c.getKickerSpeed(), kickerSpeed);
+  }
+
+  public double getFlywheelSpeed() {
+    return flywheelSpeed;
+  }
+
+  public double getKickerSpeed() {
+    return kickerSpeed;
+  }
+
+  public double getHoodPosition() {
+    return hoodPosition;
+  }
+
+  public double getAimingToleranceDeg() {
+    return aimingToleranceDeg;
+  }
+
+  public double getDistance() {
+    return distance;
+  }
 }
--- src/test/java/FiringSolutionManagerTest.java
+++ src/test/java/FiringSolutionManagerTest.java
@@ -5,59 +5,57 @@ import frc.robot.Constants;
 import frc.robot.FiringSolution.*;
 
 public class FiringSolutionManagerTest {
-    FiringSolutionManager manager;
+  FiringSolutionManager manager;
 
-    @Before
-    public void setup() {
-        manager = FiringSolutionManager.getSingleton();
-    }
+  @Before
+  public void setup() {
+    manager = FiringSolutionManager.getSingleton();
+  }
 
-    @After
-    public void shutdown() {
-        return; // nothing to shut down
-    }
+  @After
+  public void shutdown() {
+    return; // nothing to shut down
+  }
 
-    // Test a solution for an existing firing solution (should give back the same firing solution)
-    @Test 
-    public void testCalcNewSolutionExisting_insideTarmac() {
-        assertEquals(manager.calcNewSolution(Constants.FiringSolutions.insideTarmac.getDistance()),
+  // Test a solution for an existing firing solution (should give back the same firing solution)
+  @Test
+  public void testCalcNewSolutionExisting_insideTarmac() {
+    assertEquals(manager.calcNewSolution(Constants.FiringSolutions.insideTarmac.getDistance()),
         Constants.FiringSolutions.insideTarmac);
-    }
+  }
 
-    // Test a solution for a distance value that is below the shortest firing solution
-    // (should give back lowest firing solution, in this case middleTarmac)
-    @Test
-    public void testCalcNewSolutionBelowMinimum_middleTarmac() {
-        assertEquals(manager.calcNewSolution(0), Constants.FiringSolutions.middleTarmac);
-    }
+  // Test a solution for a distance value that is below the shortest firing solution
+  // (should give back lowest firing solution, in this case middleTarmac)
+  @Test
+  public void testCalcNewSolutionBelowMinimum_middleTarmac() {
+    assertEquals(manager.calcNewSolution(0), Constants.FiringSolutions.middleTarmac);
+  }
 
-    // Test a solution for a distance value that is above the longest firing solution
-    // (should give back highest firing solution, in this case reallyFar)
-    @Test
-    public void testCalcNewSolutionAboveMaximum_reallyFar() {
-        assertEquals(manager.calcNewSolution(1000), Constants.FiringSolutions.reallyFar);
-    }
+  // Test a solution for a distance value that is above the longest firing solution
+  // (should give back highest firing solution, in this case reallyFar)
+  @Test
+  public void testCalcNewSolutionAboveMaximum_reallyFar() {
+    assertEquals(manager.calcNewSolution(1000), Constants.FiringSolutions.reallyFar);
+  }
 
-    @Test
-    public void testCalcNewSolution_insideMiddleTarmac() {
-        FiringSolution insideTarmac = Constants.FiringSolutions.insideTarmac;
-        FiringSolution middleTarmac = Constants.FiringSolutions.middleTarmac;
+  @Test
+  public void testCalcNewSolution_insideMiddleTarmac() {
+    FiringSolution insideTarmac = Constants.FiringSolutions.insideTarmac;
+    FiringSolution middleTarmac = Constants.FiringSolutions.middleTarmac;
 
-        // Since this is the midpoint, find the average between the two values for the new firing solution
-        double newKickerSpeed = (insideTarmac.getKickerSpeed() + middleTarmac.getKickerSpeed()) / 2;
-        double newFlywheelSpeed = (insideTarmac.getFlywheelSpeed() + middleTarmac.getFlywheelSpeed()) / 2;
-        double newHoodPosition = (insideTarmac.getHoodPosition() + middleTarmac.getHoodPosition()) / 2;
-        double newAimingTolerance = (insideTarmac.getAimingToleranceDeg() + middleTarmac.getAimingToleranceDeg()) / 2;
-        double newDistance = (insideTarmac.getDistance() + middleTarmac.getDistance()) / 2;
+    // Since this is the midpoint, find the average between the two values for the new firing
+    // solution
+    double newKickerSpeed = (insideTarmac.getKickerSpeed() + middleTarmac.getKickerSpeed()) / 2;
+    double newFlywheelSpeed =
+        (insideTarmac.getFlywheelSpeed() + middleTarmac.getFlywheelSpeed()) / 2;
+    double newHoodPosition = (insideTarmac.getHoodPosition() + middleTarmac.getHoodPosition()) / 2;
+    double newAimingTolerance =
+        (insideTarmac.getAimingToleranceDeg() + middleTarmac.getAimingToleranceDeg()) / 2;
+    double newDistance = (insideTarmac.getDistance() + middleTarmac.getDistance()) / 2;
 
-        FiringSolution insideMiddleTarmac = new FiringSolution(
-            newKickerSpeed, 
-            newFlywheelSpeed, 
-            newHoodPosition,
-            newAimingTolerance,
-            newDistance
-        );
+    FiringSolution insideMiddleTarmac = new FiringSolution(newKickerSpeed, newFlywheelSpeed,
+        newHoodPosition, newAimingTolerance, newDistance);
 
-        assertEquals(manager.calcNewSolution(newDistance), insideMiddleTarmac);
-    }
+    assertEquals(manager.calcNewSolution(newDistance), insideMiddleTarmac);
+  }
 }
